ccbbbdf7af6460cdb38e3f8c7f33e2d3a328d12d
ydunga <55959066+ydunga@users.noreply.github.com>
2021-04-10 15:12:35 +0100

    Add files via upload

---
+++ ohmslaw.js
@@ -0,0 +1,43 @@
+function reveal() {
+    anime ({
+        targets: '#cell2',
+        opacity: 100,
+    })
+
+}
+
+function hide() {
+    anime({
+        targets: '#cell2',
+        opacity: 0,
+    })
+}
+
+function reveal2() {
+    anime ({
+        targets: '#cell3',
+        opacity: 100,
+    })
+
+}
+
+function hide2() {
+    anime({
+        targets: '#cell3',
+        opacity: 0,
+    })
+}
+function reveal3() {
+    anime ({
+        targets: '#cell4',
+        opacity: 100,
+    })
+
+}
+
+function hide3() {
+    anime({
+        targets: '#cell4',
+        opacity: 0,
+    })
+}
